1dd5655050034a64adfc926082635cafd19193ab
chrober <christoph.obermair@gmx.at>
2026-05-29 07:11:45 +0200

    Fix coding style

--- android/app/src/main/java/org/lyrion/squeezelite/PlayerService.java
+++ android/app/src/main/java/org/lyrion/squeezelite/PlayerService.java
@@ -383,7 +383,7 @@ public class PlayerService extends Service {
         stopMetadataPolling();
         stopTerminateTimer();
         lib.stopPlayer(this);
-        if (mediaSession != null) {
+        if (null!=mediaSession) {
             mediaSession.setActive(false);
             mediaSession.release();
         }
@@ -445,7 +445,7 @@ public class PlayerService extends Service {
     private void registerBtA2dpReceiver() {
         try {
             BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-            if (adapter != null) {
+            if (null!=adapter) {
                 btA2dpConnected = adapter.getProfileConnectionState(BluetoothProfile.A2DP) == BluetoothProfile.STATE_CONNECTED;
             }
         } catch (SecurityException e) {
@@ -459,8 +459,8 @@ public class PlayerService extends Service {
                     int state = intent.getIntExtra(BluetoothProfile.EXTRA_STATE, BluetoothProfile.STATE_DISCONNECTED);
                     boolean wasConnected = btA2dpConnected;
                     btA2dpConnected = (state == BluetoothProfile.STATE_CONNECTED);
-                    Utils.debug("A2DP state:" + state + ", btA2dpConnected:" + btA2dpConnected);
-                    if (btA2dpConnected && !wasConnected && currentServerAddress != null) {
+                    Utils.debug("A2DP state:"+state+", btA2dpConnected:"+btA2dpConnected);
+                    if (btA2dpConnected && !wasConnected && null!=currentServerAddress) {
                         startMetadataPolling();
                     } else if (!btA2dpConnected && wasConnected) {
                         stopMetadataPolling();
@@ -477,7 +477,7 @@ public class PlayerService extends Service {
     }
 
     private void unregisterBtA2dpReceiver() {
-        if (btA2dpReceiver != null) {
+        if (null!=btA2dpReceiver) {
             unregisterReceiver(btA2dpReceiver);
             btA2dpReceiver = null;
         }
@@ -525,7 +525,7 @@ public class PlayerService extends Service {
             int trackNum = 0;
 
             JSONArray playlistLoop = result.optJSONArray("playlist_loop");
-            if (playlistLoop != null && playlistLoop.length() > 0) {
+            if (null!=playlistLoop && playlistLoop.length() > 0) {
                 JSONObject track = playlistLoop.getJSONObject(0);
                 title = track.optString("title", "");
                 artist = track.optString("artist", "");
